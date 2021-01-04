4ab1a749810b5f70277929f699796bd851f9e139
victorchan764 <victorchan764@gmail,com>
2021-01-04 01:09:57 +0000

    Comments
    Commenting

--- middleware/passport.js
+++ middleware/passport.js
@@ -2,7 +2,6 @@ const LocalStrategy = require('passport-local').Strategy;
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
-// load user model
 const User = require('../models/Users');
 
 module.exports = function(passport){
